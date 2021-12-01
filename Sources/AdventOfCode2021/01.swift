fda6bfa59c5214e4b57d4f348a645b81d67ffc04
Luke Redpath <luke@lukeredpath.co.uk>
2021-12-01 15:32:51 +0000

    Solution 1.

--- Sources/AdventOfCode2021/01.swift
+++ Sources/AdventOfCode2021/01.swift
@@ -1,1 +1,22 @@
+import Foundation
+import Overture
 
+enum Day01 {
+    static func parseInput(lines: String) -> [Int] {
+        return lines
+            .components(separatedBy: .newlines)
+            .compactMap(Int.init)
+    }
+
+    static func analyseInput(_ input: [Int]) -> Int {
+        var increases = 0
+        for index in input[1...].indices {
+            if input[index] > input[index - 1] {
+                increases += 1
+            }
+        }
+        return increases
+    }
+
+    static let run = pipe(parseInput, analyseInput)
+}
--- Tests/AdventOfCode2021Tests/AdventOfCode2021Tests.swift
+++ Tests/AdventOfCode2021Tests/AdventOfCode2021Tests.swift
@@ -2,7 +2,9 @@
     @testable import AdventOfCode2021
 
     final class AdventOfCode2021Tests: XCTestCase {
-        func test01() {
-            XCTFail()
+        func test01() async {
+            let result = Day01.run(input_001)
+            XCTAssert(result > 0, "Expected count to be non-zero")
+            print("Number of increases: \(result)")
         }
     }
