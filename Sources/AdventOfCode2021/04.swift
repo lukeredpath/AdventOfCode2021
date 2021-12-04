4532a191bf9d0cb12a0fea13839507e5ead9ea88
Luke Redpath <luke@lukeredpath.co.uk>
2021-12-04 22:21:54 +0000

    Implement day 4 part 1 solution.

--- Sources/AdventOfCode2021/04.swift
+++ Sources/AdventOfCode2021/04.swift
@@ -9,17 +9,39 @@ enum Day04 {
         var c: T
         var d: T
         var e: T
+
         var values: [T] { [a, b, c, d, e] }
+
+        init(a: T, b: T, c: T, d: T, e: T) {
+            self.a = a
+            self.b = b
+            self.c = c
+            self.d = d
+            self.e = e
+        }
+
+        init(values: [T]) {
+            assert(values.count == 5, "Can only make a line from exactly 5 squares")
+
+            self.a = values[0]
+            self.b = values[1]
+            self.c = values[2]
+            self.d = values[3]
+            self.e = values[4]
+        }
     }
 
     struct NumberSquare {
         var number: Int
         var marked: Bool = false
+        var unmarked: Bool { !marked }
     }
 
     typealias DrawSequence = [Int]
-    typealias BingoCard = Five<Five<NumberSquare>>
+    typealias Line = Five<NumberSquare>
+    typealias BingoCard = Five<Line>
     typealias GameInput = (draw: DrawSequence, cards: [BingoCard])
+    typealias GameResult = (card: BingoCard, winningNumber: Int)
 
     static let drawParser = Many(
         Int.parser(),
@@ -74,4 +96,61 @@ enum Day04 {
         }
         return result
     }
+
+    static func transpose(_ card: BingoCard) -> BingoCard {
+        .init(
+            a: .init(values: card.values.map(\.a)),
+            b: .init(values: card.values.map(\.b)),
+            c: .init(values: card.values.map(\.c)),
+            d: .init(values: card.values.map(\.d)),
+            e: .init(values: card.values.map(\.e))
+        )
+    }
+
+    static func checkNumber(_ number: Int, card: BingoCard) -> BingoCard {
+        BingoCard(values: card.values.map { row in
+            Line(values: row.values.map { square in
+                if square.number == number {
+                    return update(square) { square in
+                        square.marked = (square.number == number)
+                    }
+                } else {
+                    return square
+                }
+            })
+        })
+    }
+
+    static func isWinningLine(_ line: Line) -> Bool {
+        line.values.allSatisfy(\.marked)
+    }
+
+    static func isWinningCard(_ card: BingoCard) -> Bool {
+        card.values.contains(where: isWinningLine) ||
+            transpose(card).values.contains(where: isWinningLine)
+    }
+
+    static func calculateWinningCardScore(card: BingoCard, winningNumber: Int) -> Int {
+        winningNumber * card.values
+            .flatMap(\.values)
+            .filter(\.unmarked)
+            .reduce(0) { $0 + $1.number }
+    }
+
+    static func runGame(input: GameInput) -> GameResult? {
+        var cards = input.cards
+        for number in input.draw {
+            cards = cards.map(with(number, curry(checkNumber)))
+            if let winningCard = cards.first(where: isWinningCard) {
+                return (winningCard, number)
+            }
+        }
+        return nil
+    }
+
+    static let partOne = pipe(
+        parseInput,
+        runGame,
+        map(calculateWinningCardScore)
+    )
 }
